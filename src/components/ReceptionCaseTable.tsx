042681cba4b716b6a0106b4d3a9c4649d2ae9742
crystal <crystal@rollspace.com>
2023-12-06 23:46:22 +0300

    finished reception table

--- src/components/ReceptionCaseTable.tsx
+++ src/components/ReceptionCaseTable.tsx
@@ -1,16 +1,31 @@
 import { receptionCasesListData } from "../lib/constant";
 import ReceptionTableListEl from "./ReceptionTableListEl";
+import Button from "./common/Button";
+import { IoPersonAddOutline } from "react-icons/io5";
+import IconButton from "./common/Button/IconButton";
+import { TbFingerprintScan } from "react-icons/tb";
 
 function ReceptionCaseTable() {
   return (
     <table className="w-full bg-white rounded-md overflow-hidden p-3">
-      <div className="flex">
-        <div className="px-4 py-6">
-            <h1 className="text-[20px]">List of cases</h1>
+      <div className="flex px-4 py-6 w-full justify-between">
+        <div className="">
+          <h1 className="text-[18px] text-text-700 font-semibold">
+            List of cases
+          </h1>
+          <p className="text-[12px] text-slate-400 mt-1">140 Available cases</p>
         </div>
- 
 
-        <div className=""></div>
+        <div prefix="" className="flex gap-3 ">
+          <Button variant="contained" className="items-center flex gap-2">
+            <IoPersonAddOutline />
+            <span>Add case</span>
+          </Button>
+
+          <IconButton variant="outline" >
+            <TbFingerprintScan/>
+          </IconButton>
+        </div>
       </div>
 
       <div className="p-4">
