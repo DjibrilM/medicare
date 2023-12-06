5945e92cc99c87ad9750f4f5608eac419e35114d
crystal <crystal@rollspace.com>
2023-12-07 00:46:03 +0300

    improved side menu

--- src/components/common/SideMenu.tsx
+++ src/components/common/SideMenu.tsx
@@ -1,16 +1,17 @@
 import { GiHospitalCross } from "react-icons/gi";
 import { GrHomeRounded } from "react-icons/gr";
-import { HiMiniUserGroup } from "react-icons/hi2";
-import { MdSupervisorAccount } from "react-icons/md";
-import { IoLeafOutline } from "react-icons/io5";
+import { PiBedLight } from "react-icons/pi";
 // import { useRecoilValue } from "recoil";
 // import userSate from "../../state/user";
 import { BsClockHistory } from "react-icons/bs";
+import { LiaBriefcaseMedicalSolid } from "react-icons/lia";
 import { ReactNode } from "react";
 import Button from "./Button";
 import { TbLogout2 } from "react-icons/tb";
 import { NavLink } from "react-router-dom";
 import { cn } from "../../util/cn";
+import { IoCalendarNumberOutline } from "react-icons/io5";
+import { AiOutlineSchedule } from "react-icons/ai";
 
 const menuElements: Record<string, () => ReactNode> = {
   reception: () => (
@@ -18,81 +19,49 @@ const menuElements: Record<string, () => ReactNode> = {
       <NavLink
         to={"/reception"}
         className={({ isActive }) =>
-          cn("hover:text-primary-500", {
-            "active-side-menu text-primary-500 font-bold ": isActive,
-            " text-text-500": !isActive,
+          cn("side-menu-item", {
+            "active-sidemu-item": isActive,
           })
         }
       >
-        <li className="flex items-center relative gap-3 pt-5">
-          <GrHomeRounded className="text-3xl" />
-          <span className="text-sm ">Home</span>{" "}
-          <div className="w-full absolute"></div>
-        </li>
+        <GrHomeRounded className="" />
+        <span className="text-sm ">Home</span>{" "}
       </NavLink>
 
       <NavLink
         to={"/doctors"}
         className={({ isActive }) =>
-          cn("hover:text-primary-500", {
-            "active-side-menu text-primary-500 font-bold": isActive,
-            " text-text-500": !isActive,
+          cn("side-menu-item", {
+            "active-sidemu-item": isActive,
           })
         }
       >
-        <li className="flex items-center relative gap-3 pt-5">
-          <MdSupervisorAccount className="text-3xl" />
-          <span className="text-sm">Doctors</span>{" "}
-          <div className="w-full absolute"></div>
-        </li>
-      </NavLink>
-
-      <NavLink
-        to={"/clients"}
-        className={({ isActive }) =>
-          cn("hover:text-primary-500", {
-            "active-side-menu text-primary-500 font-bold": isActive,
-            " text-text-500": !isActive,
-          })
-        }
-      >
-        <li className="flex items-center relative gap-3 pt-5">
-          <HiMiniUserGroup className="text-3xl" />
-          <span className="text-sm">Clients</span>{" "}
-          <div className="w-full absolute"></div>
-        </li>
+        <GrHomeRounded className="" />
+        <span className="text-sm ">Doctors</span>{" "}
       </NavLink>
 
       <NavLink
         to={"/pharmacy"}
         className={({ isActive }) =>
-          cn("hover:text-primary-500", {
-            "active-side-menu text-primary-500 font-bold": isActive,
-            " text-text-500": !isActive,
+          cn("side-menu-item", {
+            "active-sidemu-item": isActive,
           })
         }
       >
-        <li className="flex items-center relative gap-3 pt-5">
-          <IoLeafOutline className="text-3xl" />
-          <span className="text-sm">Pharmacy</span>{" "}
-          <div className="w-full absolute"></div>
-        </li>
+        <LiaBriefcaseMedicalSolid />
+        <span className="text-sm ">Pharmacy</span>{" "}
       </NavLink>
 
       <NavLink
-        to={"/pharmacy"}
+        to={"/cases-history"}
         className={({ isActive }) =>
-          cn("hover:text-primary-500", {
-            "active-side-menu text-primary-500 font-bold": isActive,
-            " text-text-500": !isActive,
+          cn("side-menu-item", {
+            "active-sidemu-item": isActive,
           })
         }
       >
-        <li className="flex items-center relative gap-3 pt-5">
-          <BsClockHistory className="text-3xl" />
-          <span className="text-sm">Cases History</span>{" "}
-          <div className="w-full absolute"></div>
-        </li>
+        <BsClockHistory />
+        <span className="text-sm ">Cases History</span>{" "}
       </NavLink>
     </>
   ),
@@ -110,8 +79,44 @@ const SideMenu = () => {
         </div>
 
         <div className="w-full h-full flex flex-col  justify-between">
-          <ul className="w-full gap-5 flex flex-col px-5 pt-10 ">
+          <ul className="w-full gap-5 flex flex-col pt-10 ">
             <MenuItems />
+            <hr />
+            <NavLink
+              to={"/apointment"}
+              className={({ isActive }) =>
+                cn("side-menu-item", {
+                  "active-sidemu-item": isActive,
+                })
+              }
+            >
+              <IoCalendarNumberOutline />
+              <span className="text-sm ">Apointment</span>{" "}
+            </NavLink>
+
+            <NavLink
+              to={"/intern"}
+              className={({ isActive }) =>
+                cn("side-menu-item", {
+                  "active-sidemu-item": isActive,
+                })
+              }
+            >
+              <PiBedLight />
+              <span className="text-sm ">Interns</span>{" "}
+            </NavLink>
+
+            <NavLink
+              to={"/schedule"}
+              className={({ isActive }) =>
+                cn("side-menu-item", {
+                  "active-sidemu-item": isActive,
+                })
+              }
+            >
+              <AiOutlineSchedule />
+              <span className="text-sm ">Schedule</span>{" "}
+            </NavLink>
           </ul>
 
           <div className="w-full flex  items-center h-20 bottom-20 absolute  px-5 ">
